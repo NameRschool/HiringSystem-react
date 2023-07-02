c1ea2fe2a3a45e51e8233b0daf75164df2df1895
Rivky Fisher <hand1b20@mbjcomp.org.il>
2023-07-02 10:31:26 +0300

    basic job crud

--- src/api/JobsApi.ts
+++ src/api/JobsApi.ts
@@ -1,38 +1,72 @@
 import axios, { AxiosResponse } from "axios"
+import { v4 as uuidv4 } from 'uuid';
 
 const baseUrl: string = "http://localhost:3002"
 
 export const getJobs = async (): Promise<AxiosResponse<IjobType>> => {
   try {
-    const jobs: AxiosResponse<IjobType> = await axios.get(
-      baseUrl + "/api/jobs"
-    )
-    return jobs
+    const response: AxiosResponse<IjobType> = await axios.get(
+      `${baseUrl}/api/jobs`
+    );
+    return response
   } catch (error) {
     throw new Error(String(error))
   }
 }
 
-
-export const addJob = async (
-  formData: IjobType
-): Promise<AxiosResponse<IjobType>> => {
+export const getJobById = async (jobId: string): Promise<AxiosResponse<IjobType>> => {
   try {
-      const job: Omit<IjobType, "_id"> = {
-      name: formData.name,
-      location: formData.location,
-      jobDescription: formData.jobDescription,
-      companyDescription: formData.companyDescription,
-      requierments: formData.requierments,
-      candidatesList: formData.candidatesList,
-    }
-    const saveJob: AxiosResponse<IjobType> = await axios.post(
-      baseUrl + "/api/jobs",
-      job
-    )
-    return saveJob
+    const response: AxiosResponse<IjobType> = await axios.get(
+      `${baseUrl}/api/jobs/${jobId}`
+    );
+    return response;
+  } catch (error) {
+    throw new Error(String(error));
+  }
+};
+
+export const createJob = async (
+  formData: Omit<IjobType, '_id' | 'status' | 'date'>
+): Promise<AxiosResponse<string>> => {
+  try {
+    const jobData: IjobType = {
+      _id: uuidv4(),
+      status: true,
+      date: new Date(),
+      ...formData,
+    };
+    const response: AxiosResponse<string> = await axios.post(
+      `${baseUrl}/api/jobs`,
+      jobData
+    );
+    return response;
+  } catch (error) {
+    throw new Error(String(error));
+  }
+};
+
+export const UpdateJobbyId = async (
+  jobId: string,
+  updatedData: Partial<IjobType>
+  ): Promise<AxiosResponse<string>> => {
+  try {
+    const response: AxiosResponse<string> = await axios.put(
+      `${baseUrl}/api/jobs${jobId}`,
+      updatedData
+    );
+    return response
   } catch (error) {
     throw new Error(String(error))
   }
 }
 
+export const deleteJobById = async (jobId: string): Promise<AxiosResponse<String>> => {
+  try {
+    const response: AxiosResponse<string> = await axios.delete(
+      `${baseUrl}/api/jobs/${jobId}`
+    );
+    return response;
+  } catch (error) {
+    throw new Error(String(error));
+  }
+};
