529b872930a92f4aaf4a3d77bf336e954c939e0f
Rivky Fisher <hand1b20@mbjcomp.org.il>
2023-07-02 09:10:59 +0300

    adding to main

---
+++ src/Thema.tsx
@@ -0,0 +1,10 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2a9461"
+    }
+  }
+});
+
+export default theme;
